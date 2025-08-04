ad01917b564acd8a018eb0d35c31a3ef5bb257e6
MarleneMaimuna <marlenemaimuna@gmail.com>
2025-08-04 02:25:28 +0200

    Add files via upload

---
+++ answers.sql
@@ -0,0 +1,9 @@
+--  1 Create a new database called salesDB
+CREATE DATABASE IF NOT EXISTS salesDB;
+-- Creates the database 'salesDB' only if it doesn't already exist, maybe because i create 2 times, i have to make sure that doesnt exist.
+
+-- 2 Drop (delete) the database called demo
+DROP DATABASE IF EXISTS demo;
+-- Drops or delete the database 'demo' only if it exists.
+
+
